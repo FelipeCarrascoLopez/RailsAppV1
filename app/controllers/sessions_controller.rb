ed60be226ba09fce3fe24c4ab5a73b23dc5238d6
FelipeCarrascoLopez <felipecarrascolopez@gmail.com>
2023-12-11 21:00:10 +0000

    modificiones

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,24 +7,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # debugger
-
     @user = User.find_by(email: params[:session][:email])
 
     if @user.present? && @user.authenticate(params[:session][:password])
       if @user.is_confirmed?
         session[:user_id] = @user.id
-        redirect_to root_path, notice: "Login successfully"
+        redirect_to root_path, notice: "Iniciaste sesion"
       else
-        redirect_to login_path, notice: "You have to confirm your account."
+        redirect_to login_path, notice: "Tienes que confirmar tu cuenta."
       end
     else
-      redirect_to login_path, notice: "Email or password incorrect"
+      redirect_to login_path, notice: "Correo electrónico o contraseña incorrectos"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path, notice: "Logout successfully"
+    redirect_to login_path, notice: "Cerraste Sesion"
   end
 end
